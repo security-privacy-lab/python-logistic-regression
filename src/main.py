47416cdb895ae969d002a577c3e578fca4a1b3d5
jalmaznaai <jalmaznaai@protonmail.com>
2024-06-09 14:11:48 -0500

    Model is now stored and loaded locally, instead of retraining every time.

--- src/main.py
+++ src/main.py
@@ -2,11 +2,31 @@ from sklearn.metrics import accuracy_score
 from sklearn.linear_model import LogisticRegression
 from logistic_regression import LogisticRegression as CustomLogisticRegression
 from data import x_train, x_test, y_train, y_test
+import os
+import pickle
+
+MODEL_DIRECTORY = '../models'
+MODEL_PATH = '../models/Custom_Logistic_Regression.pkl'
+
+print(os.path.exists(MODEL_DIRECTORY))
+
+if not os.path.exists(MODEL_PATH):
+    print(f"[MODEL NOT FOUND] No model at {MODEL_PATH}")
+    lr = CustomLogisticRegression()
+    lr.fit(x_train, y_train, epochs=150)
+
+    with open(MODEL_PATH, 'wb+') as file:
+        print(f"[SAVING MODEL] Saving model at {MODEL_PATH}")
+        pickle.dump(lr, file, pickle.HIGHEST_PROTOCOL)
+        print(f"[SAVING MODEL] Model successfully saved locally")
+else:
+    print(f"[MODEL FOUND] Found model at {MODEL_PATH}")
+    with open(MODEL_PATH, 'rb+') as file:
+        print(f"[LOADING MODEL] Attempting to load model from {MODEL_PATH}")
+        lr = pickle.load(file)
+        print(f"[LOADING MODEL] Load successful")
 
 
-
-lr = CustomLogisticRegression()
-lr.fit(x_train, y_train, epochs=150)
 pred = lr.predict(x_test)
 accuracy = accuracy_score(y_test, pred)
 print("Custom made Regression Accuracy:", accuracy)
