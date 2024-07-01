b660cd784e84bb0ba3f5d71ff33a628efe52dfbb
Joseph <jalmaznaai@protonmail.com>
2024-07-01 02:18:32 -0500

    Added a scaled x set, normalized to user with unit variance (mean of 0, SD of 1)

--- src/data/__init__.py
+++ src/data/__init__.py
@@ -1,1 +1,1 @@
-from data.loader import x_train, x_test, y_train, y_test, x, y
+from data.loader import x_train, x_test, y_train, y_test, x, y, x_scaled_train, x_scaled_test
--- src/data/loader.py
+++ src/data/loader.py
@@ -1,9 +1,9 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.datasets import load_breast_cancer
+from sklearn.preprocessing import StandardScaler
 
 import numpy as np
-from Pyfhel import *
 
 
 def sklearn_to_df(data_loader):
@@ -21,11 +21,18 @@ def sklearn_to_df(data_loader):
 
 
 
+
+
 x, y = sklearn_to_df(load_breast_cancer())
 
+
+ss = StandardScaler()
+x_scaled = pd.DataFrame(ss.fit_transform(x),columns = x.columns)
 
 
 x_train, x_test, y_train, y_test = train_test_split(
     x, y, test_size=0.2, random_state=42)
+
+x_scaled_train, x_scaled_test = train_test_split(x_scaled, test_size=0.2, random_state=42)
 
 q = 1
